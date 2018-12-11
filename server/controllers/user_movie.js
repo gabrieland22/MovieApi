19cc834d676f1cd5ce09f88388c413ba5f02a8cb
Gabriel André <desenvolvimento@fencom.com.br>
2018-12-11 16:09:54 -0200

    Ajustes Finais

--- server/controllers/user_movie.js
+++ server/controllers/user_movie.js
@@ -6,7 +6,7 @@ var UserMovieCtrl = {};
 module.exports = UserMovieCtrl;
 
 
-//GET /user/:id/statistic - informa a quantidade de horas de filme assistidas pelo usuário 
+ 
 UserMovieCtrl.statistic = function (id, callback) {
     var params = [id];
     var sql = ' SELECT user.id AS userId, SUM(movie.lenght) AS length FROM user \
@@ -29,60 +29,6 @@ UserMovieCtrl.statistic = function (id, callback) {
     });
 };
 
-//GET /user/:id/list - retorna uma lista de filmes do usuário (Assistidos, Assistir e Favoritos)
-UserMovieCtrl.readAll = function (id, callback) {
-    var params = [id];
-    var result = { userId: id };
-
-    var sql = ' SELECT movie.id, movie.title, movie.photo_url AS photoURL, movie.released_date AS releasedAt FROM user \
-                INNER JOIN usermovie ON user.id = usermovie.user_id \
-                INNER JOIN movie ON usermovie.movie_id = movie.id \
-                WHERE user.id = ? AND usermovie.watched = 1';
-
-    database.query(sql, params, 'release', function (err, rows) {
-        if (err) {
-            callback(response.error(400, err));
-            return;
-        }
-
-        result.watched = rows;
-
-        sql = ' SELECT movie.id, movie.title, movie.photo_url AS photoURL, movie.released_date AS releasedAt FROM user \
-        INNER JOIN usermovie ON user.id = usermovie.user_id \
-        INNER JOIN movie ON usermovie.movie_id = movie.id \
-        WHERE user.id = ? AND usermovie.toWatch = 1';
-
-        database.query(sql, params, 'release', function (err, rows) {
-            if (err) {
-                callback(response.error(400, err));
-                return;
-            }
-
-            result.toWatch = rows;
-
-            sql = ' SELECT movie.id, movie.title, movie.photo_url AS photoURL, movie.released_date AS releasedAt FROM user \
-            INNER JOIN usermovie ON user.id = usermovie.user_id \
-            INNER JOIN movie ON usermovie.movie_id = movie.id \
-            WHERE user.id = ? AND usermovie.favorite = 1';
-
-            database.query(sql, params, 'release', function (err, rows) {
-                if (err) {
-                    callback(response.error(400, err));
-                    return;
-                }
-
-                result.favorite = rows;
-
-                callback(response.result(200, result));
-            });
-
-        });
-
-    });
-
-};
-
-//POST /user/:id/list - inserir um filme na lista do usuário definindo se foi (Assistido, Assistir e Favorito)
 UserMovieCtrl.insert = function (userId, params, callback) {
     var watched = 0;
     var toWatch = 0;
@@ -142,7 +88,7 @@ UserMovieCtrl.insert = function (userId, params, callback) {
     });
 };
 
-//DELETE /user/:userid/list/:movieid - deletar um filme na lista do usuário
+
 UserMovieCtrl.deleteFromID = function (userId, movieId, callback) {
     var params = [userId, movieId];
     var sql = ' DELETE FROM usermovie WHERE user_id = ? AND movie_id = ? ';
@@ -156,3 +102,58 @@ UserMovieCtrl.deleteFromID = function (userId, movieId, callback) {
         callback(response.result(200));
     });
 };
+
+
+UserMovieCtrl.readAll = function (id, callback) {
+    var params = [id];
+    var result = { userId: id };
+
+    var sql = ' SELECT movie.id, movie.title, movie.photo_url AS photoURL, movie.released_date AS releasedAt FROM user \
+                INNER JOIN usermovie ON user.id = usermovie.user_id \
+                INNER JOIN movie ON usermovie.movie_id = movie.id \
+                WHERE user.id = ? AND usermovie.watched = 1';
+
+    database.query(sql, params, 'release', function (err, rows) {
+        if (err) {
+            callback(response.error(400, err));
+            return;
+        }
+
+        result.watched = rows;
+
+        sql = ' SELECT movie.id, movie.title, movie.photo_url AS photoURL, movie.released_date AS releasedAt FROM user \
+        INNER JOIN usermovie ON user.id = usermovie.user_id \
+        INNER JOIN movie ON usermovie.movie_id = movie.id \
+        WHERE user.id = ? AND usermovie.toWatch = 1';
+
+        database.query(sql, params, 'release', function (err, rows) {
+            if (err) {
+                callback(response.error(400, err));
+                return;
+            }
+
+            result.toWatch = rows;
+
+            sql = ' SELECT movie.id, movie.title, movie.photo_url AS photoURL, movie.released_date AS releasedAt FROM user \
+            INNER JOIN usermovie ON user.id = usermovie.user_id \
+            INNER JOIN movie ON usermovie.movie_id = movie.id \
+            WHERE user.id = ? AND usermovie.favorite = 1';
+
+            database.query(sql, params, 'release', function (err, rows) {
+                if (err) {
+                    callback(response.error(400, err));
+                    return;
+                }
+
+                result.favorite = rows;
+
+                callback(response.result(200, result));
+            });
+
+        });
+
+    });
+
+};
+
+
